34ca251afaeaa89821365d491f3ecad905879e7d
Kevin Firko <kfirko@bitcurve.com>
2022-11-21 15:07:44 -0500

    api delete deprecated dto classes based on class-validator

--- apps/api/src/modules/opx/dto/update-phrase-list.dto.ts
+++
@@ -1,4 +0,0 @@
-// import { PartialType } from '@nestjs/swagger'
-// import { CreatePhraseListDto } from './create-phrase-list.dto'
-
-// export class UpdatePhraseListDto extends PartialType(CreatePhraseListDto) {}
--- apps/api/src/modules/opx/dto/update-video-group.dto.ts
+++
@@ -1,4 +0,0 @@
-// import { PartialType } from '@nestjs/swagger'
-// import { CreateVideoGroupDto } from './create-video-group.dto'
-
-// export class UpdateVideoGroupDto extends PartialType(CreateVideoGroupDto) {}
