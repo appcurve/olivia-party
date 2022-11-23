f7cc0c2fa1d70e685853798061947b6afdfcd4ad
Kevin Firko <kfirko@bitcurve.com>
2022-11-23 17:32:20 -0500

    libs update react libs jest.config.ts to reflect latest generated react animated jest config / latest nx

--- libs/shared/react/forms-rhf/jest.config.ts
+++ libs/shared/react/forms-rhf/jest.config.ts
@@ -3,8 +3,8 @@ export default {
   displayName: 'shared-react-forms-rhf',
   preset: '../../../../jest.preset.js',
   transform: {
-    // added missing `presets` arg to support testing components that import from other libs in the project monorepo
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/shared/react/forms-rhf',
